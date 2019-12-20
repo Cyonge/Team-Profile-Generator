fbda1c224f9a65f0fd69f0729b90fb1d9e474417
Christian Yonge <christianyonge93@gmail.com>
2019-12-20 15:09:02 -0600

    added prompts and incorporated employee class into the app.js file

--- app.js
+++ app.js
@@ -8,14 +8,126 @@ const Manager = require("./lib/manager.js");
 // will need to have separate inquirer prompts depending on role etc. 
 
 
-var engineer = new Engineer("christian", "engineer@email.com", "developer", "githubUsername");
-console.log(engineer.getName());
+class Employee{
+    
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this.id = id;
 
-var intern = new Intern("Daniel", "intern@email.com,", "intern", "Harvard");
-console.log(intern);
+    }
+    getName(){
+        return this.name;
+    };
+    getId(){
+        return this.id;
+    };
+    getRole(){
+        return "Employee";
+    };
+    getEmail(){
+        return this.email;
+    }
 
-var manager = new Manager("Osei", "manager@email.com,", "manager", "123");
-console.log(manager.getEmail());
+
+
+
+
+
+promptUser(answers) {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "what is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ]).then(function (res) {
+        console.log(res)
+        if (res.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is your github Username?"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                }
+            ]);
+
+        } else if (res.role === "Intern") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "school",
+                    type: "input",
+                    message: "Where did you graduate from college?"
+                }
+            ]);
+        } else if (res.role === "Manager") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "office",
+                    type: "input",
+                    message: "What is your office number?"
+                }
+            ]);
+        };
+
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
+};
+
+}
+
+
+module.exports = Employee;
+
+var newEmployee = new Employee();
+
+newEmployee.promptUser();
+
+
+
+// var engineer = new Engineer("christian", "engineer@email.com", "developer", "githubUsername");
+// console.log(engineer.getName());
+
+// var intern = new Intern("Daniel", "intern@email.com,", "intern", "Harvard");
+// console.log(intern);
+
+// var manager = new Manager("Osei", "manager@email.com,", "manager", "123");
+// console.log(manager.getEmail());
 
 
 
